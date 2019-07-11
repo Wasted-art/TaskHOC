4c7e71baa6788e3459492d2cf60214a00d38c68a
Wasted-Art <wastedartz@gmail.com>
2019-07-11 02:57:14 +0200

    Landing Page
    After a long study .Net Framework, Umbraco, CMS, and IIS.
    I also looked into Azure, in order to test it for later hosting.
    I made the Header and footer and CSS.
    I Checked up on boostrap, didn't use it that much.
    Also i made the Navigation Bar.
    Next up Products on front page.

---
+++ UmbracoWebAppChr/WebApplicationCG/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("WebApplicationCG")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("WebApplicationCG")>
+<Assembly: AssemblyCopyright("Copyright ©  2019")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("f8d6d938-7c81-44eb-b08e-044c17383625")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
